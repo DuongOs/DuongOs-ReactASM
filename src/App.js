963335db60335468c6baa5a9769ffc7eebbd38b3
DuongOs <duongthfx12101@funix.edu.vn>
2021-11-10 04:44:08 +0900

    updated theme

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container-fluid">
-        <Navbar>
-          <NavbarBrand  className="col bg-primary text-light shadow-sm p-3 mb-5 bg-body">
-            <h1>Ứng dụng quản lý nhân sự v1.0</h1>
+      <div className="container-fluid bg-secondary bg-gradient">
+        <Navbar className="row">
+          <NavbarBrand  className="col-12 bg-primary text-light shadow-sm p-3 mb-5 bg-body">
+            <div className="col-12 h2">Ứng dụng quản lý nhân sự v1.0</div>
           </NavbarBrand>
         </Navbar>
         <StaffList/>
--- src/component/stafflist.js
+++ src/component/stafflist.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { STAFFS } from "../shared/staffs";
 import dateFormat from "dateformat";
+import { Button } from "reactstrap";
 
 function StaffList() {
   const [SelectedStaff, setSelectedStaff] = useState(false);
@@ -16,7 +17,13 @@ function StaffList() {
         className="fw-bolder col-sm-12 col-md-6 col-lg-4 border-radisus"
         key={staff.id}
       >
-        <div onClick={() => handleClick(staff)}>{staff.name}</div>
+        <Button onClick={() => handleClick(staff)} width="200" className="">
+          <img src={staff.image} alt="Image" width="50" height="50" />
+          <span className="h3">
+            <span> </span>
+            {staff.name}
+          </span>
+        </Button>
       </td>
     ));
 
@@ -25,41 +32,51 @@ function StaffList() {
       return <p>Nhấn vào tên nhân viên để hiển thị thông tin</p>;
     } else {
       return (
-        <div>
-          <h2>Họ và tên: {params.name}</h2>
-          <img src={params.image} />
-          <p>
-            Ngày sinh: <span> </span>
-            {dateFormat(params.doB, "dd/mm/yyyy")}
-            <br />
-            Ngày vào công ty: <span> </span>
-            {dateFormat(params.startDate, "dd/mm/yyyy")}
-            <br />
-            Phòng ban: <span> </span>
-            {params.department.name}
-            <br />
-            Số ngày nghỉ còn lại: <span> </span>
-            {SelectedStaff.annualLeave}
-            <br />
-            Số ngày đã làm thêm: <span> </span>
-            {SelectedStaff.overTime}
-            <br />
-          </p>
+        <div className="col-sm-11 col-md-10 col-lg-10 m-auto">
+          <div className="card mt-3 mb-3">
+            <h2 className="card-title">Thông tin nhân viên: {params.name}</h2>
+            <img src={params.image} className="" />
+            <div className="card-body">
+              <p className="card-text">
+                <span>
+                  Họ và tên: <span className="h5">{params.name}</span>
+                </span>
+                <br />
+                Ngày sinh: <span> </span>
+                {dateFormat(params.doB, "dd/mm/yyyy")}
+                <br />
+                Ngày vào công ty: <span> </span>
+                {dateFormat(params.startDate, "dd/mm/yyyy")}
+                <br />
+                Phòng ban: <span> </span>
+                {params.department.name}
+                <br />
+                Số ngày nghỉ còn lại: <span> </span>
+                {SelectedStaff.annualLeave}
+                <br />
+                Số ngày đã làm thêm: <span> </span>
+                {SelectedStaff.overTime}
+                <br />
+              </p>
+              <button type="button" className="btn btn-outline-primary mr-1">Giao việc</button>
+              <button type="button" className="btn btn-outline-warning ml-1">Cập nhật thông tin</button>
+            </div>
+          </div>
         </div>
       );
     }
   }
 
   return (
-    <div className="container-fluid">
-      <table>
+    <div className="pb-5 pt-5 container-fluid">
+      <table >
         <tbody>
           <tr className="row">
             <List />
           </tr>
         </tbody>
       </table>
-      <Info params={SelectedStaff} />
+      <Info params={SelectedStaff}/>
     </div>
   );
 }
