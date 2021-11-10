516146ffe315aeb0f9bb8b59e3862c1a097dbf2b
DuongOs <duongthfx12101@funix.edu.vn>
2021-11-11 04:27:54 +0900

    updated all function to named function

--- src/component/stafflist.js
+++ src/component/stafflist.js
@@ -6,36 +6,66 @@ import { Button } from "reactstrap";
 function StaffList() {
   const [SelectedStaff, setSelectedStaff] = useState(false);
   const [staffs] = useState(STAFFS);
+  const [GridClassName, setGridClassName] = useState(
+    "text-wrap fw-bolder col-md-6 col-lg-4 border-radisus mb-1"
+  );
 
-  const handleClick = (staffInfo) => {
+  function handleClick(staffInfo) {
     setSelectedStaff(staffInfo);
-  };
+  }
 
-  const List = () =>
-    staffs.map((staff) => (
-      <td
-        className="fw-bolder col-sm-12 col-md-6 col-lg-4 border-radisus"
-        key={staff.id}
-      >
-        <Button onClick={() => handleClick(staff)} width="200" className="">
-          <img src={staff.image} alt="Image" width="50" height="50" />
-          <span className="h3">
+  function GridChange() {
+
+    function SetGridAuto() {
+      setGridClassName("text-wrap fw-bolder col-auto border-radisus mb-1");
+    }
+    
+    function SetGrid2() {
+      setGridClassName("text-wrap fw-bolder col-6 border-radisus mb-1");
+    }
+    
+    function SetGrid3() {
+      setGridClassName("text-wrap fw-bolder col-4 border-radisus mb-1");
+    }
+
+    function SetGrid6() {
+      setGridClassName("text-wrap fw-bolder col-2 border-radisus mb-1");
+    }
+    
+    return (
+      <div className="mr-1 d-none d-sm-block pb-3">
+        <span className="text-primary">Đổi giao diện:  </span>
+        <Button className="btn btn-outline-primary mr-1" onClick={SetGridAuto}>Auto</Button>
+        <Button className="btn btn-outline-primary mr-1" onClick={SetGrid2}>2 cột</Button>
+        <Button className="btn btn-outline-primary mr-1" onClick={SetGrid3}>3 cột</Button>
+        <Button className="btn btn-outline-primary mr-1" onClick={SetGrid6}>6 cột</Button>
+      </div>
+    );
+  }
+
+  function List() {
+    return staffs.map((staff) => (
+      <div className={GridClassName} key={staff.id}>
+        <Button onClick={() => handleClick(staff)} className="btn btn-outline-primary display-6">
+          <img src={staff.image} alt={staff.image} width="50" height="50" />
+          <span className="text-wrap display-6">
             <span> </span>
             {staff.name}
           </span>
         </Button>
-      </td>
+      </div>
     ));
+  }
 
   function Info({ params }) {
     if (!params) {
-      return <p>Nhấn vào tên nhân viên để hiển thị thông tin</p>;
+      return <p className="text-danger">Nhấn vào tên nhân viên để hiển thị thông tin</p>;
     } else {
       return (
         <div className="col-sm-11 col-md-10 col-lg-10 m-auto">
-          <div className="card mt-3 mb-3">
-            <h2 className="card-title">Thông tin nhân viên: {params.name}</h2>
-            <img src={params.image} className="" />
+          <div className="card text-white bg-primary mb-3 mt-3">
+            <h2 className="card-title color-primary">Thông tin nhân viên: {params.name}</h2>
+            <img src={params.image} alt={params.name} />
             <div className="card-body">
               <p className="card-text">
                 <span>
@@ -58,8 +88,6 @@ function StaffList() {
                 {SelectedStaff.overTime}
                 <br />
               </p>
-              <button type="button" className="btn btn-outline-primary mr-1">Giao việc</button>
-              <button type="button" className="btn btn-outline-warning ml-1">Cập nhật thông tin</button>
             </div>
           </div>
         </div>
@@ -69,14 +97,15 @@ function StaffList() {
 
   return (
     <div className="pb-5 pt-5 container-fluid">
-      <table >
-        <tbody>
-          <tr className="row">
+      <div>
+        <GridChange id="GridChange" />
+        <div>
+          <div className="row">
             <List />
-          </tr>
-        </tbody>
-      </table>
-      <Info params={SelectedStaff}/>
+          </div>
+        </div>
+      </div>
+      <Info params={SelectedStaff} />
     </div>
   );
 }
