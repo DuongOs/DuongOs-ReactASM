38eed45b1ca2a2b7122447e098ac0b2199b74b09
DuongOs <duongthfx12101@funix.edu.vn>
2021-11-11 04:32:16 +0900

    updated thm color and other

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import { Navbar, NavbarBrand} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import StaffList from './component/stafflist';
 
@@ -8,9 +8,9 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container-fluid bg-secondary bg-gradient">
+      <div className="container bg-gradient">
         <Navbar className="row">
-          <NavbarBrand  className="col-12 bg-primary text-light shadow-sm p-3 mb-5 bg-body">
+          <NavbarBrand  className="col-12 bg-primary text-light shadow-sm p-3 mb-3 bg-body">
             <div className="col-12 h2">Ứng dụng quản lý nhân sự v1.0</div>
           </NavbarBrand>
         </Navbar>
